3fb8583b35a7dfe48747d36726a4315da5844706
Kevin Fahrenberger <fahrenberger.k@web.de>
2024-01-30 18:30:06 +0100

    feat: add button and button onclick function

--- 01_tutorial/src/Content.js
+++ 01_tutorial/src/Content.js
@@ -6,9 +6,15 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log("You clicked it");
+  };
+
   return (
     <main>
       <p>Hello {handleNameChange()}</p>
+      <button onClick={handleClick}>Press Me</button>
     </main>
   );
 };
